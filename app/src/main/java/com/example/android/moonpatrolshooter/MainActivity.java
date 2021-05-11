812d7c565c553e88fb8f58beab22b93f76ed0e50
pesu12 <sundberg_p@yahoo.com>
2021-05-11 14:42:54 +0200

    All files added

--- app/src/main/java/com/example/android/moonpatrolshooter/MainActivity.java
+++ app/src/main/java/com/example/android/moonpatrolshooter/MainActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+public class MainActivity extends Activity {
+
+    GameEngine mGameEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager()
+                .getDefaultDisplay();
+
+        Point size = new Point();
+        display.getSize(size);
+        mGameEngine = new GameEngine(this, size);
+        setContentView(mGameEngine);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGameEngine.startThread();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGameEngine.stopThread();
     }
 }
