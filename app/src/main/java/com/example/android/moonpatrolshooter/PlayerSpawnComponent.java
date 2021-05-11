4c1759e8e8ac6d9b01e0a6ad91b08895b91559dc
pesu12 <sundberg_p@yahoo.com>
2021-05-11 15:50:34 +0200

    set player y-level to ground

--- app/src/main/java/com/example/android/moonpatrolshooter/PlayerSpawnComponent.java
+++ app/src/main/java/com/example/android/moonpatrolshooter/PlayerSpawnComponent.java
@@ -2,13 +2,14 @@ package com.example.android.moonpatrolshooter;
 
 class PlayerSpawnComponent implements SpawnComponent {
 
+    static final float PLAYER_Y_POS = 750.0f;
+
     @Override
     public void spawn(Transform playerTransform, Transform t) {
 
-        // Spawn in the centre of the screen
+        // Spawn in the centre with vertical height on ground
         t.setLocation(
                 t.getmScreenSize().x/2,
-                t.getmScreenSize().y/2);
-
+                PLAYER_Y_POS);
     }
 }
